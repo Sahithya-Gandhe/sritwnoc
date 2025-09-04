96b553cb9c8e0d3a8948db24b73faa79266825c0
Sahithya-Gandhe <sahithyagandhe@gmail.com>
2025-09-04 15:50:03 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ require('dotenv').config();
 
 // Initialize Nodemailer with GoDaddy SMTP
 const transporter = nodemailer.createTransport({
-  host: 'smtpout.secureserver.net',
+  host: process.env.SMTP_HOST,
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    user: 'rudra@exoticaexperience.in',
-    pass: 'Rudrasahasra@07'
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   }
 });
 
